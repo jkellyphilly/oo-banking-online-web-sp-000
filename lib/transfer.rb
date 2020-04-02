9c3626b372138bb76e53a76e7907c4a24389034b
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-02 15:13:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,10 @@ class Transfer
 
   def execute_transaction
     if sender.balance > amount
-    sender.balance -= amount
-    receiver.balance += amount
+      sender.balance -= amount
+      receiver.balance += amount
+    else
+      puts "rejected"
   end
 
 end
